37c9d9bd5de79250f801197b6e0b99cd79fff0ca
hyerin-jang <hrjang92@gmail.com>
2022-04-10 21:27:13 +0900

    embedded type

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -18,6 +18,20 @@ public class Member extends BaseEntity  {
     @Column(name = "USERNAME")
     private String username;
 
+    @Embedded
+    private Period workPeriod;
+
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipCode", column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+
 //    @Column(name = "TEAM_ID")
 //    private Long teamId;
 
